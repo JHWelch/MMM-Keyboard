9d1c97007f1ddc4b888f991dbee2c9540e16d76c
Jordan Welch <jordanhwelch@gmail.com>
2026-09-19 10:46:14 -0500

    Test language override

--- __tests__/MMM-Keyboard.spec.js
+++ __tests__/MMM-Keyboard.spec.js
@@ -15,13 +15,28 @@ beforeEach(() => {
   MMMKKeyboard.setData({ name, identifier: `Module_1_${name}` });
 });
 
-it('has a default config', () => {
-  expect(MMMKKeyboard.defaults).toEqual({
-    showAlways: false,
-    layout: 'default',
-    language: 'en',
-    startUppercase: true,
-    startWithNumbers: false,
-    debug: false,
+describe('defaults', () => {
+  it('has a default config', () => {
+    expect(MMMKKeyboard.defaults).toEqual({
+      showAlways: false,
+      layout: 'default',
+      language: 'en',
+      startUppercase: true,
+      startWithNumbers: false,
+      debug: false,
+    });
+  });
+
+  it('uses the config language override when set', () => {
+    const previousConfig = global.config;
+    global.config = { language: 'de' };
+
+    jest.resetModules();
+    require('../MMM-Keyboard');
+    MMMKKeyboard = global.Module.create(name);
+    MMMKKeyboard.setData({ name, identifier: `Module_1_${name}` });
+
+    expect(MMMKKeyboard.defaults.language).toBe('de');
+    global.config = previousConfig;
   });
 });
